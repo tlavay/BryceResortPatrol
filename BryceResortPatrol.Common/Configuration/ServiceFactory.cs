66b819616846ae2cfa4fe7373f8344054b7d230d
Tyler La Vay <tylavay@microsoft.com>
2021-11-30 09:39:13 -0500

    remove secret

--- BryceResortPatrol.Common/Configuration/ServiceFactory.cs
+++ BryceResortPatrol.Common/Configuration/ServiceFactory.cs
@@ -21,7 +21,7 @@ namespace BryceResortPatrol.Common.Configuration
                 AllowBulkExecution = true
             };
 
-            return new CosmosClient("AccountEndpoint=https://bryce-mountain-patrol-prod1.documents.azure.com:443/;AccountKey=8kZTkRAqkGJpr1TCQOnreHe3ND9NA0NAJodf7WzVwDQk7iq8gvEIRkVkRLWPk8AwAYOwy7AdPGSsFY4Zpvo87Q==;", clientOptions);
+            return new CosmosClient(config.Cosmos.DocumentEndpoint, config.Cosmos.PrimaryMasterKey, clientOptions);
         }
     }
 }
